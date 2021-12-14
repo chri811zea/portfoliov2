366a0254edbc8cb09cce845b2e0f28093e660098
chri811zea <90614293+chri811zea@users.noreply.github.com>
2021-12-14 21:19:30 +0100

    Add files via upload
    Added first files

---
+++ app.js
@@ -0,0 +1,33 @@
+let intro = document.querySelector('.intro');
+let letter = document.querySelector('.welcome-header');
+let letterSpan = document.querySelectorAll('.letter');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    setTimeout(()=>{
+
+        letterSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx + 1) * 200)
+        });
+
+        setTimeout(()=>{
+            letterSpan.forEach((span, idx) =>{
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                },(idx + 1) * 50)
+            })
+        },2000)
+
+        setTimeout(()=>{
+            splashLogo.classList.add('active');
+        }, 2500)
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+            
+        },3500)
+    })
+})
